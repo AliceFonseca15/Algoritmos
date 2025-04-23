ac8d9384e3d1566960602df8c822a83fdc78ad0f
Alice <120612012+AliceFonseca15@users.noreply.github.com>
2025-04-23 18:30:13 +0000

    update

---
+++ Atividade 1/questão9.py
@@ -0,0 +1,12 @@
+lista = list(map(int,input().split()))
+contador = 0
+
+for i in range(0,len(lista) - 1):
+    if lista[i] > lista[i + 1]:
+        contador += 1
+
+if contador > 0:
+    print("Não está ordenado de forma  não decrescente")
+else:
+    print("Está ordenado de forma não decrescente")
+    
---
+++ Aula-15.04/ex1.cpp
@@ -0,0 +1,11 @@
+// Introdução a c++//
+#include <iostream> 
+
+int main(){
+
+    int a,b,c;
+    std::cin >> a >> b;
+    c = a + b
+    std::cout << c << std::endl;
+    return 0
+}
