c43573cbd2bf0c5c4530f1171326c019442a978e
Alice <120612012+AliceFonseca15@users.noreply.github.com>
2025-05-05 21:43:54 -0300

    eliminando arquivos vazios

--- Atividade_2/questão3.cpp
+++ Atividade_2/questão3.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
 #include <iomanip>
 
-<<<<<<< HEAD
-=======
 
->>>>>>> 6d206171aed80b52b90d670981c5c6333ebbdf5a
 float area(float diametro){
   float r = diametro / 2;
   float a = 3.14159 * (r * r);
@@ -15,12 +12,6 @@ int main(){
   std:: cin >> diametro;
   float result = area(diametro);
   std:: cout << std::fixed <<std::setprecision(5) << result;
-<<<<<<< HEAD
-  return 0;         
-
-}
-=======
   return 0;        
 
 }
->>>>>>> 6d206171aed80b52b90d670981c5c6333ebbdf5a
